0d7ee753413930c4bc303f51eb1e50cdff195bc5
Tobias Redmann <tobias.redmann@gmail.com>
2013-06-05 09:25:16 +0200

    tracking and class install routines

--- zorro.php
+++ zorro.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Plugin Name: Zorro
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
-Version: The Plugin's Version Number, e.g.: 1.0
-Author: Name Of The Plugin Author
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Plugin Name: Zorro URL Shortener
+Plugin URI: https://github.com/tobias-redmann/zorro
+Description: A URL Shortener for mascerade urls
+Version: 1.0
+Author: Tobias Redmann
+Author URI: http://www.tricd.de
+License: MIT
 */
 
 include('lib/base62-encode.php');
@@ -14,10 +14,11 @@ include('lib/base62-encode.php');
 
 register_activation_hook( __FILE__, array('Zorro', 'installZorro') );
 
-add_filter('query_vars', 'add_my_var');
+add_filter('query_vars', array('Zorro','addQueryVars'));
 
-add_action('init','wptuts_custom_tags'); 
-add_action('wp', array('Zorro', 'commandController'));
+add_action('init',  array('Zorro', 'addRewriteRule')); 
+add_action('wp',    array('Zorro', 'commandController'));
+
 
 add_shortcode('zorro', array('Zorro', 'shortcode'));
 
@@ -30,6 +31,12 @@ class Zorro {
   const TABLE_TEMP_CLICKS = 'zorro_clicks_temporary';
   
   
+  /**
+   * Returns the table name with prefix for the urls table
+   * 
+   * @global type $wpdb
+   * @return type
+   */
   static function getTableUrls() {
     
     global $wpdb;
@@ -38,9 +45,25 @@ class Zorro {
     
   }
   
+  static function getTemporaryTable() {
+    
+    global $wpdb;
+    
+    return ($wpdb->prefix . self::TABLE_TEMP_CLICKS);
+    
+  }
+  
+  
+  /**
+   * Creates the shortcode
+   * 
+   * @param type $attributes
+   * @param type $content
+   * @return type
+   */
   static function shortcode($attributes, $content) {
     
-    extract(shortcode_atts(array('url' => '/'),$attributes));
+    extract(shortcode_atts(array('url' => '/'), $attributes));
     
     $url = self::getShortUrl($url);
     
@@ -56,18 +79,15 @@ class Zorro {
   }
   
   
-  static function commandController() {
-    
-    global $wp_query;
-    
-   
+  /**
+   * Controller to handle the zorro requests
+   */
+  static function commandController() {      
     
     $command = get_query_var('zorro_cmd');
     
     
     if ($command == 'redirect') {
-      
-      #die();
       
       self::handleRedirect();
       
@@ -83,29 +103,88 @@ class Zorro {
     
     if ($url !== false) {
       
+      self::trackClick(Base62::convert($shorturl,62,10));
+      
       wp_redirect($url, 301);
       exit();
       
-    } else {
-      
-      die('555');
-      
-    }
+    } 
     
   }
   
   
   static function installZorro() {
     
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
+    
     $sql = "CREATE TABLE ". self::getTableUrls() ." (
-        id bigint(20) NOT NULL AUTO_INCREMENT,
+        id int(11) NOT NULL AUTO_INCREMENT,
         url varchar(2560) NOT NULL,
         PRIMARY KEY (id),
         KEY url (url(767)))";
 
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-  
+    
     dbDelta( $sql );
+    
+    $temporary_tracking_table = "CREATE TABLE ". self::getTemporaryTable() ." (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      url_id int(11) NOT NULL,
+      click_time datetime NOT NULL,
+      ip varchar(30),
+      referrer varchar(2560),
+      browser varchar(2560),
+      PRIMARY KEY (id)
+     ) ";
+    
+    dbDelta($temporary_tracking_table);
+    
+    
+  }
+  
+  static function trackClick($shorturl_id) {
+    
+    global $wpdb;
+    
+    $args = array();
+    
+    $types = array();
+    
+    // id
+    $args['url_id'] = $shorturl_id;
+    $types[]        = '%d';
+   
+    // click_time
+    $args['click_time'] = current_time('mysql');
+    $types[]            = '%s';
+    
+    // ip
+    if (isset($_SERVER['REMOTE_ADDR'])) {
+      
+      $args['ip'] = $_SERVER['REMOTE_ADDR'];
+      $types[]    = '%s';
+      
+    }
+    
+    // referrer
+    if (isset($_SERVER['HTTP_REFERER'])) {
+      
+      $args['referrer'] = $_SERVER['HTTP_REFERER'];
+      $types[]          = '%s';
+      
+    }
+    
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+      
+      $args['browser']  = $_SERVER['HTTP_USER_AGENT'];
+      $types[]          = '%s';
+      
+    }
+    
+    $wpdb->insert( self::getTemporaryTable(), 
+      $args, 
+      $types 
+    );
+    
     
     
   }
@@ -134,8 +213,7 @@ class Zorro {
   static function createShortUrl($url) {
     
     global $wpdb;
-    
-    
+        
     $id = self::shortUrlExists($url);
     
     
@@ -221,45 +299,34 @@ class Zorro {
   }
   
   
-}
-
-function add_my_var($public_query_vars) {
+  function addQueryVars($public_query_vars) {
     
     $public_query_vars[] = 'shorturl_id';
     $public_query_vars[] = 'zorro_cmd';
     
     return $public_query_vars;
+  }
+  
+
+
+  function addRewriteRule() {  
+  
+    add_rewrite_rule("^r/([^/])?",'index.php?zorro_cmd=redirect&shorturl_id=$matches[1]','top');  
+
+  } 
+  
+  
 }
 
 
 
 
 
-function wptuts_custom_tags() {  
-   add_rewrite_rule("^r/([^/])?",'index.php?zorro_cmd=redirect&shorturl_id=$matches[1]','top');  
-  #add_rewrite_rule("^r/([^/])?",'test.php?_cmd_=zorro_redirect&shorturl_id=$matches[1]','top');  
-}  
+
+ 
     
  
     
-
-if (!function_exists('gg')) {
-  
-  function gg($key){
-
-    if (isset($_GET) && isset($_GET[$key]) && trim($_GET[$key]) != '') {
-
-      return trim($_GET[$key]);
-
-    } else {
-
-      return false;
-
-    }
-
-  }
-
-}
 
 
 
